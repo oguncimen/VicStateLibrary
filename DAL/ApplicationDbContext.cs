4ba105f6f79783d41a6f18c9110d238575415aae
ogncmn <ogncmn@gmail.com>
2022-10-21 21:15:34 +1100

    Add Application DB context.

--- DAL/ApplicationDbContext.cs
+++ DAL/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,11 @@ namespace DAL
 {
     public class ApplicationDbContext
     {
-
+        static List<Books> bookList = new List<Books> {
+            new Books { Id = "9b0896fa-3880-4c2e-bfd6-925c87f22878" , Title = "CQRS for Dummies", IsReserved = false },
+            new Books { Id = "0550818d-36ad-4a8d-9c3a-a715bf15de76" , Title = "Visual Studio Tips", IsReserved = false },
+            new Books { Id = "8e0f11f1-be5c-4dbc-8012-c19ce8cbe8e1" , Title = "NHibernate Cookbook", IsReserved = false },
+        };
+        public static List<Books> BookList => bookList;
     }
 }
