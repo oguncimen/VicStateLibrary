9c848db0b9da31114e211a4e996c575ee81caeb8
ogncmn <ogncmn@gmail.com>
2022-10-21 21:34:11 +1100

    Add Commands response and request models.

---
+++ DAL/CQRS/Commands/Request/ReserveBookCommandRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.CQRS.Commands.Request
+{
+    public class ReserveBookCommandRequest
+    {
+        public string Id { get; set; }
+        public bool IsReserved { get; set; }
+        public Guid BookingNumber { get; set; }
+    }
+}
---
+++ DAL/CQRS/Commands/Response/ReserveBookCommandResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.CQRS.Commands.Response
+{
+    public class ReserveBookCommandResponse
+    {
+        public string Id { get; set; }
+        public bool IsReserved { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
