e49fdc2cdea49203186fb3a98838c871fa44ef1b
ogncmn <ogncmn@gmail.com>
2022-10-21 21:23:06 +1100

    Add GetAllBookQueryHandler function.

---
+++ DAL/CQRS/Handlers/QueryHandler/GetAllBooksQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using DAL.CQRS.Queries.Request;
+using DAL.CQRS.Queries.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.CQRS.Handlers.QueryHandler
+{
+    public class GetAllBooksQueryHandler
+    {
+        public List<GetAllBooksQueryResponse> GetAllBooks(GetAllBooksQueryRequest getAllBooksQueryRequest)
+        {
+            return ApplicationDbContext.BookList.Select(book => new GetAllBooksQueryResponse
+            {
+                Id = book.Id,
+                Title = book.Title,
+                IsReserved = book.IsReserved,
+            }).ToList();
+        }
+    }
+}
