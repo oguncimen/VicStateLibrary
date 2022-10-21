b2a6b2b09817922f31b75dc040a20f94bfb86c0f
ogncmn <ogncmn@gmail.com>
2022-10-21 21:35:08 +1100

    Add ReserveBookCommandHandler function.

--- DAL/CQRS/Handlers/CommandHandlers/ReserveBookCommandHandler.cs
+++ DAL/CQRS/Handlers/CommandHandlers/ReserveBookCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAL.CQRS.Commands.Request;
+using DAL.CQRS.Commands.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,14 @@ namespace DAL.CQRS.Handlers.CommandHandlers
 {
     public class ReserveBookCommandHandler
     {
+        public ReserveBookCommandResponse ReserveBook(ReserveBookCommandRequest reserveBookCommandRequest)
+        {
+            return new ReserveBookCommandResponse
+            {
+                Id = reserveBookCommandRequest.Id,
+                IsReserved = reserveBookCommandRequest.IsReserved,
+                IsSuccess = true,
+            };
+        }
     }
 }
