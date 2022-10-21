570add40869419c69eb2cb789a69b86453def016
ogncmn <ogncmn@gmail.com>
2022-10-21 21:41:42 +1100

    Add ReserveBook JsonResult to controller for ajax call from view.

--- VicStateLibrary/Controllers/HomeController.cs
+++ VicStateLibrary/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using DAL.CQRS.Handlers.QueryHandler;
+﻿using DAL.CQRS.Commands.Request;
+using DAL.CQRS.Handlers.QueryHandler;
 using DAL.CQRS.Queries.Request;
 using DAL.CQRS.Queries.Response;
 using System;
@@ -21,6 +22,27 @@ namespace VicStateLibrary.Controllers
         {
             List<GetAllBooksQueryResponse> allBooks = _getAllBooksQueryHandler.GetAllBooks(requestModel);
             return View(allBooks);
-        }           
+        }
+        [HttpPost]
+        public JsonResult ReserveBook()
+        {
+            try
+            {
+                var id = Request["id"];
+                var bookingNumber = Guid.NewGuid();
+
+                ReserveBookCommandRequest book = new ReserveBookCommandRequest
+                {
+                    Id = id,
+                    IsReserved = true,
+                    BookingNumber = bookingNumber,
+                };
+                return Json(book, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
